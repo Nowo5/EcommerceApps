5c52d03c63ea424a9c9f603dd5dc86a518160e06
Nowo5 <hernowo.android@gmail.com>
2018-04-29 18:17:39 +0700

    Android

--- app/src/main/java/com/andronity/ecommerceapps/MainActivity.kt
+++ app/src/main/java/com/andronity/ecommerceapps/MainActivity.kt
@@ -2,11 +2,21 @@ package com.andronity.ecommerceapps
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.andronity.ecommerceapps.Helper.ViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setContent()
+    }
+    private fun setContent(){
+        val vpa = ViewPagerAdapter(supportFragmentManager)
+        vpa.addFragment(SigninFragment(),"Sign In")
+        vpa.addFragment(SignupFragment(), "Sign Out")
+        viewPager.adapter = vpa
+        tab.setupWithViewPager(viewPager)
     }
 }
